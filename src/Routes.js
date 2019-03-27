4ba4c6201f317ca79380b5c17b6fdfe778b8b8b0
George Downe <georgedowne06@gmail.com>
2019-03-27 14:15:39 +0000

    update

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import {
-	Route
+    Route
 } from 'react-router-dom';
-import { ProtectedRoute } from './Protected.route';
-import { UnprotectedRoute } from './Unprotected.route';
-import { TraineeprotectedRoute } from './Traineeprotected.route';
+import {ProtectedRoute} from './Protected.route';
+import {UnprotectedRoute} from './Unprotected.route';
+import {TraineeprotectedRoute} from './Traineeprotected.route';
 
 import Homepage from './components/HomepageComponent';
 import SubmitForm from './components/FormComponent';
@@ -17,37 +17,34 @@ import Cohort from './components/SingleCohortComponent';
 import User from './components/SingleUserComponent';
 import ViewForm from './components/ViewFormComponent';
 import NewCohort from './components/NewCohortComponent';
-import TrainerForm from './components/TrainerFormComponent';
 
 class Routes extends Component {
-	render() {
-		return (
-			<div>
-				<Route exact path="/" component={Homepage} />
-				<Route exact path="/home" component={Homepage} />
-				{/* <TraineeprotectedRoute path="/form" component={SubmitForm} />
-				<ProtectedRoute path="/cohorts" component={Cohorts} />
-				<ProtectedRoute path="/trainees" component={Trainees} />
-				<ProtectedRoute path="/account" component={Account} />
-				<UnprotectedRoute path="/register" component={Register} />  */}
-				<Route path="/form" component={SubmitForm} />
-				<Route path="/cohorts" component={Cohorts} />
-				<Route path="/trainees" component={Trainees} />
-				<Route path="/account" component={Account} />
-				<Route path="/register" component={Register} />
-				<Route path="/trainerForm" component={TrainerForm} />
-				<Route path="/login" component={Login} />
-				{/* <ProtectedRoute path="/singlecohort/:id" component={Cohort} />
-				<ProtectedRoute path="/singleuser/:id" component={User} />
-				<ProtectedRoute path="/viewform/:id" component={ViewForm} />
-				<ProtectedRoute path="/newcohort" component={NewCohort} /> */}
-				<Route path="/singlecohort/:id" component={Cohort} />
-				<Route path="/singleuser/:id" component={User} />
-				<Route path="/viewform/:id" component={ViewForm} />
-			  <Route path="/newcohort" component={NewCohort} />
-			</div>
-		)
-	}
+  render() {
+  	return (
+  	<div>
+		<Route exact path="/" component={ Homepage } />
+		<Route exact path="/home" component={ Homepage } />
+		{/* <TraineeprotectedRoute path="/form" component={ SubmitForm } />
+		<ProtectedRoute  path="/cohorts" component={ Cohorts } />
+		<ProtectedRoute  path="/trainees" component={ Trainees } />
+		<ProtectedRoute path="/account" component={ Account } />
+		<UnprotectedRoute path="/register" component={ Register } /> */} 
+		<Route path="/form" component={ SubmitForm } />
+		<Route  path="/cohorts" component={ Cohorts } />
+		<Route  path="/trainees" component={ Trainees } />
+		<Route path="/account" component={ Account } />
+		<Route path="/register" component={ Register } /> 
+		<Route path="/login" component={ Login } />
+		{/* <ProtectedRoute  path="/singlecohort/:id" component={ Cohort } />
+		<ProtectedRoute  path="/singleuser/:id" component={ User } />
+		<ProtectedRoute  path="/viewform/:id" component={ ViewForm } />
+		<ProtectedRoute  path="/newcohort" component={ NewCohort } /> */}
+		<Route  path="/singlecohort/:id" component={ Cohort } />
+		<Route  path="/singleuser/:id" component={ User } />
+		<Route  path="/viewform/:id" component={ ViewForm } />
+		<Route  path="/newcohort" component={ NewCohort } /> 
+	</div>
+	)}
 }
 
 export default Routes;
