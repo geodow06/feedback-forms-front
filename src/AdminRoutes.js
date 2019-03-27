2b1289b3ec4c2a86a4e18d278b0dd20431fb1900
Scowy <thomas.scowcroft@ntlworld.com>
2019-03-27 16:20:53 +0000

    Created routes

---
+++ src/AdminRoutes.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {
+    Route
+} from 'react-router-dom';
+import {ProtectedRoute} from './Protected.route';
+import {UnprotectedRoute} from './Unprotected.route';
+import {TraineeprotectedRoute} from './Traineeprotected.route';
+
+import Homepage from './components/HomepageComponent';
+import SubmitForm from './components/FormComponent';
+import Cohorts from './components/CohortsComponent';
+import Trainees from './components/TraineesComponent.1';
+import Account from './components/AccountComponent';
+import Register from './components/RegisterComponent';
+import Login from './components/LoginComponent';
+import Cohort from './components/SingleCohortComponent';
+import User from './components/SingleUserComponent';
+import ViewForm from './components/ViewFormComponent';
+import NewCohort from './components/NewCohortComponent';
+
+class AdminRoutes extends Component {
+  render() {
+  	return (
+  	<div>
+		<Route exact path="/" component={ Homepage } />
+		<Route exact path="/home" component={ Homepage } />
+		{/* <TraineeprotectedRoute path="/form" component={ SubmitForm } />
+		<ProtectedRoute  path="/cohorts" component={ Cohorts } />
+		<ProtectedRoute  path="/trainees" component={ Trainees } />
+		<Route path="/form" component={ SubmitForm } />
+		<Route  path="/cohorts" component={ Cohorts } />
+		<Route  path="/trainees" component={ Trainees } />
+		<Route path="/account" component={ Account } />
+		{/* <ProtectedRoute  path="/singlecohort/:id" component={ Cohort } />
+		<ProtectedRoute  path="/singleuser/:id" component={ User } />
+		<ProtectedRoute  path="/viewform/:id" component={ ViewForm } />
+		<ProtectedRoute  path="/newcohort" component={ NewCohort } /> */}
+		<Route  path="/singlecohort/:id" component={ Cohort } />
+		<Route  path="/singleuser/:id" component={ User } />
+		<Route  path="/viewform/:id" component={ ViewForm } />
+		<Route  path="/newcohort" component={ NewCohort } /> 
+	</div>
+	)}
+}
---
+++ src/components/LandingComponent.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class LandingComponent extends Component {
+    render() {
+        return (
+            <div className="main-body">
+                <div className="home-body">
+                    <div id="dashboard-item-1">
+                        <a href="/register">REGISTER</a>
+                    </div>
+                    <div id="right-dashboard">
+                        <div id="dashboard-item-2-not-logged">
+                            <a href="/login">LOGIN</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default LandingComponent;
