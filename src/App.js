f030720bec36e3a33353e7edff97d5535570a357
George Downe <georgedowne06@gmail.com>
2019-03-28 09:15:19 +0000

    Revert "Revert "Merge pull request #3 from geodow06/AddLanding""
    This reverts commit d047c8bc1c75aac05f804836c1da42912b13d0f4.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import {	  
-    BrowserRouter as Router
+import {
+	BrowserRouter as Router
 } from 'react-router-dom';
 import Routes from './Routes';
 import Navbar from './components/NavbarComponent';
+import LandingComponent from './components/LandingComponent';
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-	      <div className="App">
-	        <header className="App-header">
-	          <Navbar />
-	        </header>
-	        <div id="main-content">
-				<Routes />
-			</div>
-	      </div>
-      </Router>
-    );
-  }
+	render() {
+		return (
+			<LandingComponent />
+		);
+	}
 }
 
 export default App;
