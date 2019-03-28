335ae08455ba24c1e511746f67b6c3645fa2e1e3
George Downe <georgedowne06@gmail.com>
2019-03-28 09:48:12 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 import Routes from './Routes';
 import Navbar from './components/NavbarComponent';
 import LandingComponent from './components/LandingComponent';
+import TrainerForm from './components/TrainerFormComponent'
 
 class App extends Component {
 	render() {
